538a301dc7681be8b6b6c828bfad74bc67ef06e4
Asadbek <schwarzenegger.asa@gmail.com>
2025-05-23 15:36:28 +0500

    Initial commit

---
+++ src/test/java/uz/pdp/lionestatebot/LionEstateBotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.lionestatebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionEstateBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
