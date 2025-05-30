f862b10dda04c30debbe84952f0aecb09f5a421c
Suhrob <suhrob_kalandarov@outlook.com>
2025-05-30 11:17:08 +0500

    Fixed `LanguageHomeService`: replace `sendCompanyPhotoMessage` with `sendMarkupMessage`, simplify language menu handling, and remove unused code and error handling.

--- src/main/java/uz/pdp/lionestatebot/bot/process/callback/LanguageHomeService.java
+++ src/main/java/uz/pdp/lionestatebot/bot/process/callback/LanguageHomeService.java
@@ -45,10 +45,9 @@ public class LanguageHomeService implements BiConsumer<CallbackQuery, Session> {
                         Messages.LANG_SET_SUCCESS_MSG.get(language)
                 );
 
-                SendResponse response = messageSender.sendCompanyPhotoMessage(
+                SendResponse response = messageSender.sendMarkupMessage(
                         session.getUserId(),
-                        Messages.ABOUT_COMPANY_MSG.get(language)
-                                + "\n\n" + Messages.CHOOSE_MENU_MSG.get(language),
+                        Messages.CHOOSE_MENU_MSG.get(language),
                         replyButtonService.homeBtns(language)
                 );
 
@@ -70,15 +69,6 @@ public class LanguageHomeService implements BiConsumer<CallbackQuery, Session> {
         Language language = session.getLanguage();
 
         if (state.equals(SessionState.START) || state.equals(SessionState.LANG_MENU)) {
-
-            try {
-
-                messageSender.sendRemoveReplyMarkup(userId, Messages.UPDATING_MSG.get(language));
-
-            } catch (Exception e) {
-                System.err.println("exp: " + e);
-            }
-
             SendResponse response = messageSender.sendInlineMarkupMessage(
                     userId,
                     "Choose language:",
@@ -90,10 +80,6 @@ public class LanguageHomeService implements BiConsumer<CallbackQuery, Session> {
             return;
         }
 
-       /* messageSender.sendRemoveReplyMarkup(userId, Messages.WELCOME_BACK_MSG.get(language)
-                + "\n\n" + Messages.ABOUT_COMPANY_MSG.get(language)
-        );
-*/
         messageSender.sendCompanyPhotoMessage(userId,
                 Messages.WELCOME_BACK_MSG.get(language)
                         + "\n\n" + Messages.ABOUT_COMPANY_MSG.get(language),
